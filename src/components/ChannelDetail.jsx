f8ecc006f56ddc64c279769167be72aea0b12e52
Mufaddal KC <mufazzalkc@gmail.com>
2024-10-10 17:47:20 +0530

    some changes

--- src/components/ChannelDetail.jsx
+++ src/components/ChannelDetail.jsx
@@ -6,34 +6,39 @@ import { Videos, ChannelCard } from "./";
 
 const ChannelDetail = () => {
   const { id } = useParams();
-  
+
   const [channelDetail, setChannelDetail] = useState(null);
   const [videos, setVideos] = useState([]);
 
   useEffect(() => {
-    fetchFromAPI(`channels?part=snippet&id=${id}`)
-      .then((data) => setChannelDetail(data?.items[0]));
-    
-    fetchFromAPI(`search?.channelId=${id}&part=snippet&order=date`)
-      .then((data) => setVideos(data?.items));
+    fetchFromAPI(`channels?part=snippet&id=${id}`).then((data) =>
+      setChannelDetail(data?.items[0])
+    );
+
+    fetchFromAPI(`search?.channelId=${id}&part=snippet&order=date`).then(
+      (data) => setVideos(data?.items)
+    );
   }, [id]);
 
   return (
-    <Box minHeight='95vh'>
+    <Box minHeight="95vh">
       <Box>
-        <div style={{
-          height:'300px',
-          background: 'linear-gradient(90deg, rgba(0,238,247,1) 0%, rgba(206,3,184,1) 100%, rgba(0,212,255,1) 100%)',
-          zIndex: 10,
-        }} />
+        <div
+          style={{
+            height: "300px",
+            background:
+              "linear-gradient(90deg, rgba(0,238,247,1) 0%, rgba(206,3,184,1) 100%, rgba(0,212,255,1) 100%)",
+            zIndex: 10,
+          }}
+        />
         <ChannelCard channelDetail={channelDetail} marginTop="-110px" />
       </Box>
-      <Box display='flex' p="2">
-        <Box sx={{ mr: { sm: '100px' } }} />
+      <Box display="flex" p="2">
+        <Box sx={{ mr: { sm: "100px" } }} />
         <Videos videos={videos} />
       </Box>
     </Box>
-  )
-}
+  );
+};
 
-export default ChannelDetail
+export default ChannelDetail;
